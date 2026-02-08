cbe6560892a8cc7b6f058a644aecd960525e0a62
dreamgineer <me@dgnr.us>
2026-02-09 05:37:34 +0700

    now remove the secrets

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,6 @@ export default defineConfig({
   schema: "./src/lib/server/db/schema.ts",
   dialect: "turso",
   dbCredentials: {
-    url: process.env.TURSO_DATABASE_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN!,
+    url: "file:data/turso.db",
   },
 });
