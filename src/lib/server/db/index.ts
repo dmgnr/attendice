0038dafb79cb15f9f9b0ff3eb76f09e9aa0ee059
dreamgineer <me@dgnr.us>
2026-02-16 15:34:02 +0700

    drizzle-seed

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,12 +1,33 @@
 import { mkdir } from "node:fs/promises";
 import { drizzle } from "drizzle-orm/libsql";
 import { migrate } from "drizzle-orm/libsql/migrator";
+import { seed } from "drizzle-seed";
+import * as schema from "./schema";
 
 await mkdir("data").catch(() => {});
 export const db = drizzle({
   connection: {
     url: "file:data/turso.db",
   },
+  schema,
 });
 
 await migrate(db, { migrationsFolder: "drizzle" });
+// @ts-expect-error stupid ts
+await seed(db, { students: schema.students }).refine((f) => ({
+  students: {
+    columns: {
+      id: f.int({
+        minValue: 1000000000,
+        maxValue: 9999999999,
+        isUnique: true,
+      }),
+      name: f.fullName(),
+      room: f.int({
+        minValue: 11,
+        maxValue: 69,
+      }),
+    },
+    count: 2919,
+  },
+}));
